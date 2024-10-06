8ce5289595b6ac1c6cfb4753d6b7c06d6cea3520
NikolasPasichnik <nikolas.pasichnik@mail.mcgill.ca>
2024-10-06 02:49:45 -0400

    Added logic for handling multiple records

--- dnsClient.py
+++ dnsClient.py
@@ -3,7 +3,7 @@ import random
 import socket
 import time
 
-global request_type, header_temp, question_temp, labels
+global request_type, header_temp, question_temp
 labels = []
 
 def parseInput():
@@ -35,7 +35,7 @@ def createDnsQuery(args):
     RD = '1'
     RA = '0'
     Z = '000'
-    RCODE = '0000' #response
+    RCODE = '0000' 
     second_header_row = str(hex(int(QR + OPCODE + AA + TC + RD + RA + Z + RCODE, 2)).replace('0x','').zfill(4))
     QDCOUNT = '0001'
     ANCOUNT = '0000'
@@ -124,11 +124,6 @@ def sendQuery(query, args):
     parseResponse(response)     
     return response     
 
-####################################### THE FUN STUFF STARTS HERE ########################################################
-
-# I am a very modular person so I created 3 new functions for decoding
-# I added a new global variable: labels
-
 # Function that decodes a label (not a pointer)
 def decode_label(rdata, index): 
     #ex : 03 77 77 77 -> www
@@ -140,57 +135,67 @@ def decode_label(rdata, index):
         char = chr(int(rdata[index:index + 2], 16))  # Decode each character
         label += char # Add each char to the label string to create the word
         index += 2  # Next character 
-
+    
     return label, index # returning the index because we want to continue from where we left off in the next label 
 
 # Function that decodes pointers 
 def decode_pointer(response, offset): 
-    global labels
     
     name = response[offset*2:] # find the starting point of the name stored in the pointer in the entire response (ex: go back to qname)
     # message can basically be for ex: qname + the rest. Cant set an end index cuz we dont know how long the name stored in the pointer is 
     index = 0
-
+    temp_label = []
     while name[index:index+2] != "00": # 00 indicates end of name
-       label, index = decode_label(name, index) # decode each label in the stored name
-       labels.append(label)
+        
+        # A nested pointer was found - must recursively run the decode_pointer function to obtain 
+        if name[index:index+2]  == "c0":
+           offset = name[index+2:index+4] # find hex offset (10)
+           offset = int(offset, 16) # convert into decimal (16)
+           temp_label += decode_pointer(response, offset)
+           break 
+        
+        # It's a simple(uncompressed) label which needs to be decoded
+        else: 
+            label, index = decode_label(name, index) # decode each label in the stored name
+            temp_label.append(label)
+
+    return temp_label
 
 # Main logic for decoding the name
 def decodeName(rdata, response):
-    global labels
     i = 0
+    labels_test = []
 
-    while i < len(rdata): # len(rdata) =  rdlength * 2 
+    while (True): 
+
+        # Compression identified, deal with pointers
         if rdata[i:i+2] == "c0": #if position starts with c0 -> pointer so the next byte is the offset. ex: c010
             offset = rdata[i+2:i+4] # find hex offset (10)
             offset = int(offset, 16) # convert into decimal (16)
-            decode_pointer(response, offset)
+            labels_test += decode_pointer(response, offset)
             i += 4 # next index after the entire pointer (ex: c010)
-        else: #if not pointer -> normal decoding
+            break
+        # Uncompressed, decode label as is 
+        elif rdata[i:i+2] != "00": #if not pointer & not zero-octet -> it's a normal label 
             label, i = decode_label(rdata, i)
-            labels.append(label)
-
-    return ".".join(labels) # connect the labels with periods label1.label2.etc
+            labels_test.append(label)
+        # elif i >= len(rdata): #not sure if this piece matters, test and then remove if useless
+        #     break 
+        else: #Case where it's the zero octet (need to increment index) 
+            i+= 2
+            break 
+        
+    return ".".join(labels_test), i # connect the labels with periods label1.label2.etc
 
 def parseResponse(response): 
     global question_temp, header_temp
 
-    print("Response: " + response) 
-    #print(len(response))
-
     # ========================================= Retrieving the Header =========================================
     header = response[0:24]
-    #print("Header: \n"+header)
-    # print(len(header))
-    # Getting the ANCOUNT, NSCOUNT and ARCOUNT
-    ID = header[0:4]
+    ID = header[0:4] #need to check response id matches with query 
 
     second_row = header[4:8]
-    # print("Second Row: "+second_row)
     second_row = bin(int(second_row, 16)).replace('0b','')
-    # print(len(second_row))
-    # print(second_row)
-    # Breaking down the second row further: 
     QR = second_row[0:1] #somewhat important 
     OPCODE = second_row[1:5]
     AA = second_row[5:6] #important for authoritative or not 
@@ -204,73 +209,70 @@ def parseResponse(response):
     RA = second_row[8:9] #sure recursive queries 
     Z = second_row[9:12]
     RCODE = second_row[12:16] #important yes 
-    # print(QR + "" + OPCODE + "" + AA + "" +  TC + "" +  RD + "" + RA + "" + Z + "" +  RCODE )
 
     QDCOUNT = header[8:12]
     ANCOUNT = header[12:16]
     NSCOUNT = header[16:20]
     ARCOUNT = header[20:24]
 
-    # print("ID: "+ID)
-    # print("QDCOUNT: "+QDCOUNT)
-    # print("ANCOUNT: "+ANCOUNT)
-    # print("NSCOUNT: "+NSCOUNT)
-    # print("ARCOUNT: "+ARCOUNT)
-
     # =========================================== Retrieving the question ===========================================
     # The length of the question depends on the length of the website name!!
+    # (hardcoded 24 -> header is always 24)
     question = response[24:24+(len(question_temp))] 
-    # print("Question: \n"+question)
 
     # ============================================= Retrieving the answer =============================================
-    if (int(ANCOUNT,16) != 0):
+    if (int(ANCOUNT,16) != 0): #technically this if is not needed 
         print(f'***Answer Section ({int(ANCOUNT,16)} records)***')
-        # Possible while loop here (see TODO)
+
+        record_offset = 0 
+        records_treated = 0 
+        # This is the WHOLE answer block (skipping header and question)
         answer = response[24+(len(question_temp)):]
-        print("Answer: "+answer)
-        # print(len(answer))
-        pointer = answer[0:4]
-        # print("pointer-test: "+str(bin(int(pointer, 16))).replace('0b',''))
-        response_type = answer[4:8]
-        response_class = answer[8:12]
-        TTL = answer[12:20]
-        ttl = int(TTL, 16)
-        RDLENGTH = answer[20:24]
-        rdlength_int=int(RDLENGTH, 16)
-        rdata = answer[24:24+rdlength_int*2]
-        '''print("Pointer: "+pointer)
-        print("Response Type: "+response_type)
-        print("Response Class: "+response_class)
-        print("TTL: "+TTL)
-        #print("RDLENGTH: " + str(rdlength_int))
-        print("IP: "+rdata)'''
 
-        if response_type == "0001":
-            # ---- Type A query ----
-            # Converting the IP 
-            # Assume IP = a.b.c.d
-            ip_a = str(int(rdata[0:2],16))
-            ip_b = str(int(rdata[2:4],16))
-            ip_c = str(int(rdata[4:6],16))
-            ip_d = str(int(rdata[6:8],16))
-            ip_full = ip_a+"."+ip_b+"."+ip_c+"."+ip_d
-            #print("IP Address = "+ip_a+"."+ip_b+"."+ip_c+"."+ip_d)
+        while records_treated < int(ANCOUNT,16): 
+            # Getting the data from beginning of the Answer section until the end 
+            rdata = answer[record_offset:] #for NAME section 
+            _, end = decodeName(rdata, response)
 
-            print(f"IP \t {ip_full} \t {ttl} \t {auth}")
-        elif response_type == "0002":
-            # Type NS query
-            alias = decodeName(rdata, response)
-            print(f"NS \t {ip_full} \t {ttl} \t {auth}")
-        elif response_type == "0005":
-            # Type CNAME
-            alias = decodeName(rdata, response)
-            print(f"CNAME \t {alias} \t {ttl} \t {auth}")
-        elif response_type == "000f":
-            # Type MX query
-            alias = decodeName(rdata, response)
-            print(f"IP \t {ip_full} \t {ttl} \t {auth}")
-        else:
-            print("error")
+            response_type = answer[record_offset+end:record_offset+end+4]
+            TTL = answer[record_offset+ end +8:record_offset+ end+16]
+            ttl = int(TTL, 16)
+            RDLENGTH = answer[record_offset+ end +16:record_offset + end+20]
+            rdlength_int=int(RDLENGTH, 16)
+            rdata = answer[record_offset+ end +20:record_offset + end+ 20+rdlength_int*2]
+
+            #This is the block that represents the entire answer data for one answer record 
+            answer_record = answer[record_offset: record_offset + end + 20+rdlength_int*2]
+
+            if response_type == "0001":
+                # ---- Type A query ----
+                # Converting the IP 
+                # Assume IP = a.b.c.d
+                ip_a = str(int(rdata[0:2],16))
+                ip_b = str(int(rdata[2:4],16))
+                ip_c = str(int(rdata[4:6],16))
+                ip_d = str(int(rdata[6:8],16))
+                ip_full = ip_a+"."+ip_b+"."+ip_c+"."+ip_d
+
+                print(f"IP \t {ip_full} \t {ttl} \t {auth}")
+            elif response_type == "0002":
+                # Type NS query
+                alias, _ = decodeName(rdata, response)
+                print(f"NS \t {alias} \t {ttl} \t {auth}")
+            elif response_type == "0005":
+                # Type CNAME
+                alias, _ = decodeName(rdata, response)
+                print(f"CNAME \t {alias} \t {ttl} \t {auth}")
+            elif response_type == "000f":
+                # Type MX query
+                alias, _ = decodeName(rdata, response)
+                print(f"IP \t {alias} \t {ttl} \t {auth}")
+            else:
+                print("error")
+            
+            # Updating offset 
+            record_offset = record_offset + len(answer_record)
+            records_treated += 1 
 
    
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -278,17 +280,15 @@ def parseResponse(response):
     '''
     TODO
 
-    - Ex for Type A, there are 2 records in Answer. Currenty we handle one.
-        - Possible logic: Add while loop in answer section that loops twice (aka for the same number of records present). 
-        - Need to make sure that we parse the second answer correctly cuz rn we only look at the first. Everything is hardcoded at the moment
-        - Ex: 
-            Complete Answer section: c00c000500010000080a000e087765626361636865026974c010c02b00010001000000e5000484d8b19d
-            Answer 1: c00c000500010000080a000e087765626361636865026974c010 (the one we have handled)
-            Answer 2: c02b00010001000000e5000484d8b19d (type is A so there must be a second line that prints the IP)
-    
     -  Output correct error messages for RCODE (p2-3 of dnsprimer)    
 
     -  rdata for type MX has a different format so need to look into this
+
+    -  If there are records in the "Additional" section, then we must also print them (idk what we can use to test this, but it's part of the doc) 
+
+    -  Some more error handling maybe? Could be more robust 
+
+    -  finalize + cleanup?? im not sure if we have anything else to do 
     
     '''
 
